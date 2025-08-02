a59e4d38e427209e04fc2f9d2d90f64b4858c8b3
Abdullah-ShahAlam-Dev <abdullahshahalam2003@gmail.com>
2025-08-02 20:46:44 +0500

    Each Asssigmeht Include 3 chapters

---
+++ Module-2 JavaScipt/JS Assigments/Assigment 1/chapter2.js
@@ -0,0 +1,62 @@
+// ------------------------------Q1
+var username;
+
+
+// ------------------------------Q2
+var myname ="Abdullah Shah Alam"
+
+
+// ------------------------------Q3
+// Declaring Varible of JS and Also assigning value calling from message
+var message = "Hello World";
+
+// 3c) Display the message in alert box
+alert("Q3\n"+message);
+
+
+
+// ------------------------------Q4
+// 4. Student's bio data in JS variables
+var studentName = "Abdullah Shah Alam";
+var studentAge = "22 years old";
+var studentCourse = "Certified Mobile Application Development";
+
+// Show the data in alert boxes
+alert("Q4\n"+studentName);
+alert("Q4\n"+studentAge);
+alert("Q4\n"+studentCourse);
+
+
+// ------------------------------Q5
+// 5. Display the following alert using one JS variable
+var txt = "PIZZA";
+txt += "\n" + txt.slice(0, 4);
+txt += "\n" + txt.slice(0, 3);
+txt += "\n" + txt.slice(0, 2);
+txt += "\n" + txt.slice(0, 1);
+alert("Q5\n"+txt)
+
+
+
+// ------------------------------Q6
+// 6. Declare email variable and show in alert
+var email = "abdullahshahalam2003@gmail.com";
+alert("Q6\n"+"My email address is " + email);
+
+
+// ------------------------------Q7
+// 7. Declare book variable and display message
+var book = "A smarter way to learn JavaScript  ";
+alert("Q7\n"+"I am trying to learn from the book " + book);
+
+
+// ------------------------------Q8
+//8. Display in browser through JS
+document.writeln("Q8\n"+"Yah! I can write HTML content through JavaScript<br>");
+
+
+// ------------------------------Q9
+// 9. Store string and show in alert and browser
+var displayText = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬”";
+alert("Q9\n"+displayText);
+document.writeln("Q9\n"+displayText);
