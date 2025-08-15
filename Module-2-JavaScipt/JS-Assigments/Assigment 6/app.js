85cf06259f016706bd200b381bdb13a4483eb2c6
Abdullah-ShahAlam-Dev <abdullahshahalam2003@gmail.com>
2025-08-15 17:35:21 +0500

    updating Assigment 6

---
+++ Module-2-JavaScipt/JS-Assigments/Assigment 6/app.js
@@ -0,0 +1,57 @@
+//********************Chapter 15-17 Array 
+
+//--------------------Q1
+//A literal is a fixed value you write directly in your code — not computed, not stored in a variable name. It’s the raw value itself.
+
+// 1. Literal notation (empty array)
+let studentsLiteral = [];
+
+// 2. Object notation (empty array)
+let studentsObject = new Array();
+
+// 3. String array
+let fruits = ["Apple", "Banana", "Cherry"];
+
+// 4. Number array
+let primeNumbers = [2, 3, 5, 7, 11];
+
+// 5. Boolean array
+let flags = [true, false, true];
+
+// 6. Mixed array
+let mixedData = ["Hello", 42, true, null];
+
+
+
+
+//--------------------Q8
+let students = ["Ali", "Ahmed", "Sara"];
+let scores = [400, 350, 480];
+let totalMarks = 500;
+
+document.write(`${students[0]} scored ${scores[0]} out of ${totalMarks} → ${(scores[0]/totalMarks)*100}%<br>`);
+document.write(`${students[1]} scored ${scores[1]} out of ${totalMarks} → ${(scores[1]/totalMarks)*100}%<br>`);
+document.write(`${students[2]} scored ${scores[2]} out of ${totalMarks} → ${(scores[2]/totalMarks)*100}%<br>`);
+
+
+
+//--------------------Q
+
+
+
+
+//--------------------Q11
+let cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+let selectedCities = cities.slice(1, 4);
+document.write("Cities List: " + cities + "<br>");
+document.write("Selected Cities: " + selectedCities);
+
+
+
+//--------------------Q
+//--------------------Q
+//--------------------Q
+//--------------------Q
+//--------------------Q
+//--------------------Q
+//--------------------Q
