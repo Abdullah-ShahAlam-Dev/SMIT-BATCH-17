e90d75361d124007ee8a5451f43b798ebc390de2
1000 <1000@NK.Iqra.Local>
2025-08-29 15:23:32 +0500

    In process

--- Module-2-JavaScipt/JS-Assigments/Assigment 9/app.js
+++ Module-2-JavaScipt/JS-Assigments/Assigment 9/app.js
@@ -1,14 +1,52 @@
 //********************Chapter 21-25: ********************//
 
 //---------------------Q1
-document.write("<h2>Q1: </h2>")
-var firstName = prompt("Enter your First Name");
-var lastName = prompt("Enter your Last Name");
-var fullName = firstName + " " + lastName;
-document.write("Hello " + fullName + "<br>");
+document.write("<h2>Q1:Positive Integer </h2>")
+var num = +prompt("Enter a positive Number");
+document.write("Number: " + num + "<br>");
+document.write("Round off value: " + Math.round(num) + "<br>");
+document.write("Floor value: " + Math.floor(num) + "<br>");
+document.write("Ceil value: " + Math.ceil(num) + "<br>");
 
 //---------------------Q2
+document.write("<h2>Q2: Negative Floating Point Number</h2>");
+var neg_num = +prompt("Enter a negative floating point number:");
+document.write("Number: " + neg_num + "<br>");
+document.write("Round off value: " + Math.round(neg_num) + "<br>");
+document.write("Floor value: " + Math.floor(neg_num) + "<br>");
+document.write("Ceil value: " + Math.ceil(neg_num) + "<br>");
+
+//---------------------Q3
 document.write("<h2>Q2: </h2>");
-var mobile = prompt("Enter your favorite mobile phone model");
-document.write("My favorite phone is: " + mobile + "<br>");
-document.write("Length of string: " + mobile.length + "<br>");
+var abs_num = +prompt("Enter a number for finding its absolute:");
+document.write("The Absolute Value of "+abs_num+"  is: " + Math.abs(abs_num) + "<br>");
+
+//---------------------Q4
+document.write("<h2>Q2: </h2>");
+
+
+
+//---------------------Q5
+document.write("<h2>Q2: </h2>");
+
+
+//---------------------Q6
+document.write("<h2>Q2: </h2>");
+
+
+//---------------------Q7
+document.write("<h2>Q2: </h2>");
+
+
+
+//---------------------Q8
+document.write("<h2>Q2: </h2>");
+
+
+//---------------------Q9
+document.write("<h2>Q2: </h2>");
+
+
+//---------------------Q10
+document.write("<h2>Q2: </h2>");
+
