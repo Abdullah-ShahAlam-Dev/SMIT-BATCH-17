2ca934a52b287df03234e60bd97e507c623a250e
Abdullah-ShahAlam-Dev <abdullahshahalam2003@gmail.com>
2025-08-29 10:54:58 +0500

    Initializing Assigment 9

---
+++ Module-2-JavaScipt/JS-Assigments/Assigment 9/app.js
@@ -0,0 +1,14 @@
+//********************Chapter 21-25: ********************//
+
+//---------------------Q1
+document.write("<h2>Q1: </h2>")
+var firstName = prompt("Enter your First Name");
+var lastName = prompt("Enter your Last Name");
+var fullName = firstName + " " + lastName;
+document.write("Hello " + fullName + "<br>");
+
+//---------------------Q2
+document.write("<h2>Q2: </h2>");
+var mobile = prompt("Enter your favorite mobile phone model");
+document.write("My favorite phone is: " + mobile + "<br>");
+document.write("Length of string: " + mobile.length + "<br>");
